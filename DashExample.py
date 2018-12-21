374a070b0946d24cfcd7b9c346b804e6d638af69
jvNiekerk <38180842+jvNiekerk@users.noreply.github.com>
2018-12-21 09:11:19 +0100

    Update DashExample.py

--- DashExample.py
+++ DashExample.py
@@ -7,6 +7,8 @@ external_stylesheets = ['https://codepen.io/chriddyp/pen/bWLwgP.css']
 
 app = dash.Dash(__name__, external_stylesheets=external_stylesheets)
 
+server = app.server # the Flask app
+
 app.layout = html.Div(children=[
         
     #Example of html header     
