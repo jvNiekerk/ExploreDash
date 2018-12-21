46d299ac121c4b14bf6873e2245c9e8bf72d63a7
jvNiekerk <38180842+jvNiekerk@users.noreply.github.com>
2018-12-21 08:27:38 +0100

    Add example dash to test MS Azure deployment.

---
+++ DashExample.py
@@ -0,0 +1,106 @@
+# -*- coding: utf-8 -*-
+import dash
+import dash_core_components as dcc
+import dash_html_components as html
+
+external_stylesheets = ['https://codepen.io/chriddyp/pen/bWLwgP.css']
+
+app = dash.Dash(__name__, external_stylesheets=external_stylesheets)
+
+app.layout = html.Div(children=[
+        
+    #Example of html header     
+    html.H1(children='Hello Dash'),
+
+    #Example of low level html row
+    html.Div(children='''
+        Dash: A web application framework for Python.
+    '''),
+    
+    #Example of Bar Graph
+    dcc.Graph(
+        id='example-graph',
+        figure={
+            'data': [
+                {'x': [1, 2, 3], 'y': [4, 1, 2], 'type': 'bar', 'name': 'SF'},
+                {'x': [1, 2, 3], 'y': [2, 4, 5], 'type': 'bar', 'name': u'Montréal'},
+            ],
+            'layout': {
+                'title': 'Dash Data Visualization Check'
+            }
+        }
+    ),
+
+    #Example of low level html row
+    html.Div(children='''
+        Example of a drop down box
+    '''),
+
+    #Example of drop down
+    dcc.Dropdown(
+        options=[
+            {'label': 'New York City', 'value': 'NYC'},
+            {'label': 'Montréal', 'value': 'MTL'},
+            {'label': 'San Francisco', 'value': 'SF'}
+        ],
+        value='MTL'
+   ),
+    
+    #Example of low level html row
+    html.Div(children='''
+        Example of a drop down box - multiple selection
+    '''),
+    
+    #Example of drop down - multiple
+    dcc.Dropdown(
+    options=[
+        {'label': 'New York City', 'value': 'NYC'},
+        {'label': 'Montréal', 'value': 'MTL'},
+        {'label': 'San Francisco', 'value': 'SF'}
+    ],
+    multi=True,
+    value="MTL"
+),
+    
+    #Example of low level html row
+    html.Div(children='''
+        Example of input box
+    '''),
+     
+    dcc.Input(
+    placeholder='Enter a value...',
+    type='text',
+    value=''
+),
+    
+    #Example of low level html row
+    html.Div(children='''
+        Example of check box
+    '''),
+             
+    dcc.Checklist(
+    options=[
+        {'label': 'New York City', 'value': 'NYC'},
+        {'label': 'Montréal', 'value': 'MTL'},
+        {'label': 'San Francisco', 'value': 'SF'}
+    ],
+    values=['MTL', 'SF']
+)
+   
+    
+    
+    
+    
+    
+    
+    
+    
+    
+    
+    
+    
+
+])
+
+if __name__ == '__main__':
+    app.run_server(debug=True)
